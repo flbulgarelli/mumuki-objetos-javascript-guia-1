7936af780307a032b058861c6c71fa80d1135707
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-20 17:30:13 -0300

    Corrigiendo error de sintaxis

--- 00040_PepitaLargaDistancia/test.js
+++ 00040_PepitaLargaDistancia/test.js
@@ -1,23 +1,23 @@
 describe("PepitaLargaDistancia", function() {
     context('desde buenosAires', function() {
         before(function() { pepita.ubicacionActual = buenosAires });
-        it(function() { assert.equal(pepita.distanciaA(buenosAires), 0); }
-        it(function() { assert.equal(pepita.distanciaA(santaFe), 407); }
-        it(function() { assert.equal(pepita.distanciaA(cordoba), 898); }
+        it(function() { assert.equal(pepita.distanciaA(buenosAires), 0); });
+        it(function() { assert.equal(pepita.distanciaA(santaFe), 407); });
+        it(function() { assert.equal(pepita.distanciaA(cordoba), 898); });
     });
 
     context('desde santaFe', function() {
         before(function() { pepita.ubicacionActual = santaFe });
-        it(function() { assert.equal(pepita.distanciaA(buenosAires), 407); }
-        it(function() { assert.equal(pepita.distanciaA(santaFe), 0); }
-        it(function() { assert.equal(pepita.distanciaA(cordoba), 491); }
+        it(function() { assert.equal(pepita.distanciaA(buenosAires), 407); });
+        it(function() { assert.equal(pepita.distanciaA(santaFe), 0); });
+        it(function() { assert.equal(pepita.distanciaA(cordoba), 491); });
     });
 
 
     context('desde cordoba', function() {
         before(function() { pepita.ubicacionActual = cordoba });
-        it(function() { assert.equal(pepita.distanciaA(buenosAires), 898); }
-        it(function() { assert.equal(pepita.distanciaA(santaFe), 491); }
-        it(function() { assert.equal(pepita.distanciaA(cordoba), 0); }
+        it(function() { assert.equal(pepita.distanciaA(buenosAires), 898); });
+        it(function() { assert.equal(pepita.distanciaA(santaFe), 491); });
+        it(function() { assert.equal(pepita.distanciaA(cordoba), 0); });
     });
 });
